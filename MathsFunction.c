7238f6e4702c8f6dd776b2a55d0339d90f3de314
Mohammed <P16176629@dmu.ac.uk>
2018-03-07 15:41:14 +0000

    Added MathsFunction

---
+++ MathsFunction.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+
+ printf("Input a float value: ");
+
+ printf("The square root of  is n");
+
+ printf(" to the 3rd power is n");
+
+ printf("The floor offn");
+
+ printf("And the ceiling of is n");
+printf("And Hello");
+
+}
